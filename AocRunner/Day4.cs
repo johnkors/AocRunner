1894878a4cdd42b8c891d17f8555f5207f6cd45c
John Korsnes <johnkors@gmail.com>
2022-12-05 10:36:37 +0100

    Day4: cleanup

--- AocRunner/Day4.cs
+++ AocRunner/Day4.cs
@@ -2,43 +2,50 @@ public class Day4 : IDaySolver
 {
     public string SolvePart1(string[] loadedInput)
     {
-        var count = 0;
-        foreach (string row in loadedInput)
-        {
-            string[] strings = row.Split(',');
-            (Range r1, Range r2) = (ToRange(strings[0]), ToRange(strings[1]));
-            if (RangeContainsRange(r1, r2) || RangeContainsRange(r2, r1))
-                count++;
-        }
+        int count = loadedInput.GetCountOf(FullOverlaps);
         return count.ToString();
     }
 
     public string SolvePart2(string[] loadedInput)
     {
-        var count = 0;
-        foreach (string row in loadedInput)
-        {
-            string[] strings = row.Split(',');
-            (Range r1, Range r2) = (ToRange(strings[0]), ToRange(strings[1]));
-            if (StartsWithin(r1, r2) || StartsWithin(r2, r1))
-                count++;
-        }
+        var count = loadedInput.GetCountOf(PartialOverlaps);
         return count.ToString();
     }
 
-    private Range ToRange(string str)
-    {
-        string[] rangeStr = str.Split('-');
-        return new Range(int.Parse(rangeStr[0]), int.Parse(rangeStr[1]));
-    }
-
-    static bool RangeContainsRange(Range r1, Range r2)
+    static bool FullOverlaps(Range r1, Range r2)
     {
         return r1.Start.Value <= r2.Start.Value && r2.End.Value <= r1.End.Value;
     }
 
-    static bool StartsWithin(Range r1, Range r2)
+    static bool PartialOverlaps(Range r1, Range r2)
     {
         return r1.Start.Value <= r2.Start.Value && r2.Start.Value <= r1.End.Value;
+    }
+}
+
+static class Day4Extensions
+{
+    public static int GetCountOf(this string[] rows, Func<Range, Range, bool> rangeCheck)
+    {
+        var count = 0;
+        foreach (string row in rows)
+        {
+            (Range r1, Range r2) = ToRanges(row);
+            if (rangeCheck(r1, r2) || rangeCheck(r2, r1))
+                count++;
+        }
+        return count;
+    }
+
+    private static (Range r1, Range r2) ToRanges(string row)
+    {
+        string[] strings = row.Split(',');
+        return (ToRange(strings[0]), ToRange(strings[1]));
+    }
+
+    private static Range ToRange(string str)
+    {
+        string[] rangeStr = str.Split('-');
+        return new Range(int.Parse(rangeStr[0]), int.Parse(rangeStr[1]));
     }
 }
