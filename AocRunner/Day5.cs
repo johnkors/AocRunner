d703c5ca308be19aa56a97046c0be9b9930b35fd
John Korsnes <johnkors@gmail.com>
2022-12-11 22:12:01 +0100

    Day5: Simplify by using better datastructure as input to algo

--- AocRunner/Day5.cs
+++ AocRunner/Day5.cs
@@ -4,106 +4,48 @@ public class Day5 : IDaySolver
 {
     public string SolvePart1(string[] loadedInput)
     {
-        var stacks = loadedInput.GetStackMapUntil(string.Empty);
+        var stacks = loadedInput.GetStacks(string.Empty);
         var directionsList = loadedInput.GetDirections();
 
         foreach (var directions in directionsList)
         {
-            new StackCraneMove().ModifyStacks(directions, stacks);
+            int amount = directions.Amount;
+
+            while (amount > 0)
+            {
+                stacks[directions.To].Push(stacks[directions.From].Pop());
+                amount--;
+            }
         }
 
-        return string.Join("", stacks.OrderBy(c => c.Key).Select(s => s.Value.Peek()));
+        return string.Join("", stacks.Select(s => s.Value.Peek()));
     }
 
     public string SolvePart2(string[] loadedInput)
     {
-        var stacks = loadedInput.GetStackMapUntil(string.Empty);
+        var stacks = loadedInput.GetStacks(string.Empty);
         var directionsList = loadedInput.GetDirections();
-
+        
         foreach (var directions in directionsList)
         {
-            new QueueCraneMove().ModifyStacks(directions, stacks);
+            int amount = directions.Amount;
+            
+            var stack = new Stack<char>();
+            
+            while (amount > 0)
+            {
+                stack.Push(stacks[directions.From].Pop());
+                amount--;
+            }
+
+            while(stack.TryPop(out var crate))
+            {
+                stacks[directions.To].Push(crate);
+            }
         }
 
-        return string.Join("", stacks.OrderBy(c => c.Key).Select(s => s.Value.Peek()));
+        return string.Join("", stacks.Select(s => s.Value.Peek()));
     }
-}
-
-
-internal abstract class CraneMove
-{
-    protected abstract void Pickup(char crate);
-    protected abstract char PutDown();
-    protected abstract int Count { get; }
-    
-    public void ModifyStacks((int Amount, int From, int To) direction, IDictionary<int, Queue<char>> stacks)
-    {
-        int directionAmount = direction.Amount;
-
-        while (directionAmount > 0)
-        {
-            Pickup(stacks[direction.From].Dequeue());
-            directionAmount--;
-        }
-
-        var newTo = new Queue<char>();
-
-        while (Count > 0)
-        {
-            newTo.Enqueue(PutDown());
-        }
-
-        while (stacks[direction.To].Count > 0)
-        {
-            newTo.Enqueue(stacks[direction.To].Dequeue());
-        }
-
-        stacks[direction.To] = newTo;
-    }
-}
-
-internal class QueueCraneMove : CraneMove
-{
-    private readonly Queue<char> _queue;
-
-    public QueueCraneMove()
-    {
-        _queue = new Queue<char>();
-    }
-
-    protected override void Pickup(char crate)
-    {
-        _queue.Enqueue(crate);
-    }
-
-    protected override char PutDown()
-    {
-        return _queue.Dequeue();
-    }
-
-    protected override int Count => _queue.Count;
-}
-
-internal class StackCraneMove : CraneMove
-{
-    private readonly Stack<char> _stack;
-
-    public StackCraneMove()
-    {
-        _stack = new Stack<char>();
-    }
-
-    protected override void Pickup(char crate)
-    {
-        _stack.Push(crate);
-    }
-
-    protected override char PutDown()
-    {
-        return _stack.Pop();
-    }
-
-    protected override int Count => _stack.Count;
 }
 
 public static class InputExtensions
@@ -126,10 +68,9 @@ public static class InputExtensions
         }
     }
 
-    
-    public static IDictionary<int, Queue<char>> GetStackMapUntil(this IEnumerable<string> rows, string row)
+    public static IDictionary<int, Stack<char>> GetStacks(this IEnumerable<string> rows, string row)
     {
-        var stackMap = new Dictionary<int, Queue<char>>();
+        var listMap = new Dictionary<int, List<char>>();
         using IEnumerator<string> enumerator = rows.AsEnumerable().GetEnumerator();
         while (enumerator.MoveNext())
         {
@@ -140,12 +81,12 @@ public static class InputExtensions
                 if (i % 4 == 0 && !char.IsWhiteSpace(dork))
                 {
                     int stackNo = i / 4;
-                    if (!stackMap.ContainsKey(stackNo))
+                    if (!listMap.ContainsKey(stackNo))
                     {
-                        stackMap.Add(stackNo, new Queue<char>());
+                        listMap.Add(stackNo, new List<char>());
                     }
                     char crateContent = prepended[i-1];
-                    stackMap[stackNo].Enqueue(crateContent);
+                    listMap[stackNo].Add(crateContent);
                 }
                 i++;
             }
@@ -156,6 +97,18 @@ public static class InputExtensions
             }
         }
 
+        var stackMap = new Dictionary<int, Stack<char>>();
+        foreach (KeyValuePair<int,List<char>> queueMapEntry in listMap.OrderBy(c => c.Key))
+        {
+            var stack = new Stack<char>();
+            var list = queueMapEntry.Value;
+            list.Reverse();
+            foreach (char crate in list)
+            {
+                stack.Push(crate);
+            }
+            stackMap.Add(queueMapEntry.Key, stack);
+        }
         return stackMap;
     }
 }
