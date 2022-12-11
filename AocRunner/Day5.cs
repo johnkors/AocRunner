b7c199937dc01343e0ab9138268bd30c6f8edcb3
John Korsnes <johnkors@gmail.com>
2022-12-11 19:31:18 +0100

    Day5: cleanup

--- AocRunner/Day5.cs
+++ AocRunner/Day5.cs
@@ -11,43 +11,50 @@ public class Day5 : IDaySolver
         foreach (var direction in directions)
         {
             int directionAmount = direction.Amount;
-            // Framework.Logger("EXECUTING DIRECTIONS!");
-            // Framework.Logger($"moving {direction.Amount} from {direction.From} to {direction.To}\n");
-            // Framework.Logger("stack " +direction.From  + ":\n[" + string.Join("]\n[", stacks[direction.From].ToArray()) + "]");
-            // Framework.Logger("stack " +direction.To  + ":\n[" + string.Join("]\n[", stacks[direction.To].ToArray()) + "]\n");
-
+         
+            var newTo = new Queue<char>();
+            var tmpStack = new Stack<char>();
+            
             while (directionAmount > 0)
             {
-                // Framework.Logger("STATUS:");
-                // Framework.Logger($"Moving 1 from Stack " +direction.From  + ":\n[" + string.Join("]\n[", stacks[direction.From].ToArray()) + "]");
-                // Framework.Logger("To Stack " +direction.To  + ":\n[" + string.Join("]\n[", stacks[direction.To].ToArray()) + "]\n");
-                if (stacks[direction.From].Count > 0)
-                {
-                    var crate = stacks[direction.From].Dequeue();
-
-                    var newTo = new Queue<char>();
-                    newTo.Enqueue(crate);
-                    while(stacks[direction.To].Count > 0)
-                    {
-                        var dequeue = stacks[direction.To].Dequeue();
-                        newTo.Enqueue(dequeue);
-                    }
-
-                    stacks[direction.To] = newTo;
-                    // Framework.Logger($"*Moving [{crate}]*");
-                }
-
-                // Framework.Logger($"*NO CRATE TO MOVE. Stack {direction.From} was empty");
+                tmpStack.Push(stacks[direction.From].Dequeue());
                 directionAmount--;
             }
-            // Framework.Logger($"\n{new string('*', 60)}");
-            // Framework.Logger("  AFTER:");
-            // Framework.Logger("  New Stack " +direction.From  + ":\n  [" + string.Join("]\n  [", stacks[direction.From].ToArray()) + "]");
-            // Framework.Logger("  New Stack " +direction.To  + ":\n  [" + string.Join("]\n  [", stacks[direction.To].ToArray()) + "]\n");
-            // Framework.Logger($"{new string('*', 60)}");
+
+            while(tmpStack.Count > 0)
+            {
+                newTo.Enqueue(tmpStack.Pop());
+            }
+            
+            while(stacks[direction.To].Count > 0)
+            {
+                newTo.Enqueue(stacks[direction.To].Dequeue());
+            }
+            
+            stacks[direction.To] = newTo;
+            
         }
 
         return string.Join("", stacks.OrderBy(c => c.Key).Select(s => s.Value.Count > 0 ? s.Value.Peek() : ' '));
+    }
+
+    private static void LogBeforeStatus((int Amount, int From, int To) direction, IDictionary<int, Queue<char>> stacks)
+    {
+        Framework.Logger("EXECUTING DIRECTIONS!");
+        Framework.Logger($"moving {direction.Amount} from {direction.From} to {direction.To}\n");
+        Framework.Logger("stack " + direction.From + ":\n[" + string.Join("]\n[", stacks[direction.From].ToArray()) + "]");
+        Framework.Logger("stack " + direction.To + ":\n[" + string.Join("]\n[", stacks[direction.To].ToArray()) + "]\n");
+    }
+
+    private static void LogAfterStatus((int Amount, int From, int To) direction, IDictionary<int, Queue<char>> stacks)
+    {
+        Framework.Logger($"\n{new string('*', 60)}");
+        Framework.Logger("  AFTER:");
+        Framework.Logger("  New Stack " + direction.From + ":\n  [" +
+                         string.Join("]\n  [", stacks[direction.From].ToArray()) + "]");
+        Framework.Logger("  New Stack " + direction.To + ":\n  [" + string.Join("]\n  [", stacks[direction.To].ToArray()) +
+                         "]\n");
+        Framework.Logger($"{new string('*', 60)}");
     }
 
     public string SolvePart2(string[] loadedInput)
@@ -59,38 +66,21 @@ public class Day5 : IDaySolver
         foreach (var direction in directions)
         {
             int directionAmount = direction.Amount;
-            Framework.Logger("EXECUTING DIRECTIONS!");
-            Framework.Logger($"moving {direction.Amount} from {direction.From} to {direction.To}\n");
-            Framework.Logger("stack " +direction.From  + ":\n[" + string.Join("]\n[", stacks[direction.From].ToArray()) + "]");
-            Framework.Logger("stack " +direction.To  + ":\n[" + string.Join("]\n[", stacks[direction.To].ToArray()) + "]\n");
-            var crates9001 = new List<char>();
+            
+            var newTo = new Queue<char>();
             
             while (directionAmount > 0)
             {
-                crates9001.Add(stacks[direction.From].Dequeue());
+                newTo.Enqueue(stacks[direction.From].Dequeue());
                 directionAmount--;
             }
 
-            var stack = new Stack<char>(new Queue<char>());
-            var newTo = new Queue<char>();
-            
-            foreach (var cratesToEnqueue in crates9001)
+            while(stacks[direction.To].Count > 0)
             {
-                newTo.Enqueue(cratesToEnqueue);
-            }
-
-            foreach (var oldTo in stacks[direction.To])
-            {
-                newTo.Enqueue(oldTo);
+                newTo.Enqueue(stacks[direction.To].Dequeue());
             }
 
             stacks[direction.To] = newTo;
-             
-            Framework.Logger($"\n{new string('*', 60)}");
-            Framework.Logger("  AFTER:");
-            Framework.Logger("  New Stack " +direction.From  + ":\n  [" + string.Join("]\n  [", stacks[direction.From].ToArray()) + "]");
-            Framework.Logger("  New Stack " +direction.To  + ":\n  [" + string.Join("]\n  [", stacks[direction.To].ToArray()) + "]\n");
-            Framework.Logger($"{new string('*', 60)}");
         }
 
         return string.Join("", stacks.OrderBy(c => c.Key).Select(s => s.Value.Count > 0 ? s.Value.Peek() : ' '));
